ee1f2510556c8d03931844ef681baaf0dc5372f7
arkady <arkadiy.korsakov.98@mail.ru>
2025-03-22 19:11:14 +0700

    completed task2 v2

--- routes/index.js
+++ routes/index.js
@@ -2,8 +2,20 @@ const express = require('express')
 
 const router = express.Router({ mergeParams: true })
 
-router.get('/rooms', (req, res) => {})
-router.get('/rooms/free', (req, res) => {})
-router.get('/rooms/:id', (req, res) => {})
-router.get('/me/rooms', (req, res) => {})
-router.get('/room-bookings/:id', (req, res) => {})
+router.get('/rooms', (req, res) => {
+  res.status(200).json({ messege: 'Все номера' })
+})
+router.get('/rooms/free', (req, res) => {
+  res.status(200).json({ messege: 'Свободные номера' })
+})
+router.get('/rooms/:id', (req, res) => {
+  res.status(200).json({ messege: `Номер ${req.params.id}` })
+})
+router.get('/me/rooms', (req, res) => {
+  res.status(200).json({ messege: 'Забронированные клиентом номера' })
+})
+router.get('/room-bookings/:roomId', (req, res) => {
+  res.status(200).json({ messege: `Все брони номера ${req.params.roomId}` })
+})
+
+module.exports = router
