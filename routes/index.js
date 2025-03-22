d1c93aafe8557f355f74b784c2ce0f256fb3946a
arkady <arkadiy.korsakov.98@mail.ru>
2025-03-22 18:49:01 +0700

    completed task3

--- routes/index.js
+++ routes/index.js
@@ -7,3 +7,7 @@ router.get('/rooms/free', (req, res) => {})
 router.get('/rooms/:id', (req, res) => {})
 router.get('/me/rooms', (req, res) => {})
 router.get('/room-bookings/:id', (req, res) => {})
+
+router.post('/auth/register', (req, res) => {})
+router.post('/auth/login', (req, res) => {})
+router.post('/room-bookings/:id', (req, res) => {})
